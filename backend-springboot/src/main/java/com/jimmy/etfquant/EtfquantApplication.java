0fff943908a9f878b24aa5adda5a0959fb02889e
a88019401 <a88019401@yahoo.com.tw>
2026-05-11 21:01:05 +0800

    Initialize Spring Boot backend service

---
+++ backend-springboot/src/main/java/com/jimmy/etfquant/EtfquantApplication.java
@@ -0,0 +1,13 @@
+package com.jimmy.etfquant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtfquantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtfquantApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/jimmy/etfquant/EtfquantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmy.etfquant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtfquantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
